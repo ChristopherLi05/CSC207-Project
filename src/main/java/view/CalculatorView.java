1035a0590ad36cb7e2b6fd09fd5e302ffa0b41e8
david-703 <jsdavid.won@gmail.com>
2024-11-15 14:20:16 -0500

    Changed import statements for CalculatorView and implemented constructor for CalculatorView.

--- src/main/java/view/CalculatorView.java
+++ src/main/java/view/CalculatorView.java
@@ -4,21 +4,27 @@ import app.IApp;
 import entity.calculator.mahjong.MahjongGroup;
 import entity.calculator.mahjong.MahjongTile;
 import view.component.ITileSelectorMaster;
-import view.component.MahjongTileInputButton;
+import view.component.DisplayHandComponent;
 import view.component.TileSelectorComponent;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
+import javax.swing.*;
+import java.awt.*;
 
 public class CalculatorView extends AbstractPanel implements ITileSelectorMaster {
     private final TileSelectorComponent tileSelectorComponent;
+    private final DisplayHandComponent displayHandComponent;
 
     public CalculatorView(IApp master) {
         super(master);
+        setLayout(new BorderLayout());
 
+        // Initialize and add DisplayHandComponent at top
+        displayHandComponent = new DisplayHandComponent();
+        add(displayHandComponent, BorderLayout.NORTH);
+
+        // Initialize and add TileSelectorComponent at center
         tileSelectorComponent = new TileSelectorComponent(this);
+        add(tileSelectorComponent, BorderLayout.CENTER);
     }
 
     @Override
