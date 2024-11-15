2f3dada63f322e8ad5fa69d5e64a64024301e6a7
ChristopherLi05 <admissions.christopher@gmail.com>
2024-11-15 16:03:29 -0500

    add temp api data accessor

---
+++ src/main/java/data_access/APIDataAccessor.java
@@ -0,0 +1,27 @@
+package data_access;
+
+import use_case.leaderboard.LeaderboardEntry;
+
+import java.util.List;
+
+public class APIDataAccessor implements IDataAccessor {
+    @Override
+    public boolean signUp(String username, String password) {
+        return false;
+    }
+
+    @Override
+    public String logIn(String username, String password) {
+        return "";
+    }
+
+    @Override
+    public List<LeaderboardEntry> getTopTenLeaderboard() {
+        return List.of();
+    }
+
+    @Override
+    public void updateScore(String name, int score) {
+
+    }
+}
