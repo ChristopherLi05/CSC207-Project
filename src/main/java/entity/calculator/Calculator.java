9f020c9be1df82443b7faac5144211aedc7c9a75
fuma <fuma.kano@mail.utoronto.ca>
2024-11-18 15:20:42 -0500

    Implemented methods for calculator, modified MahjongTile implementations. Still need modifications on Calculator.java

--- src/main/java/entity/calculator/Calculator.java
+++ src/main/java/entity/calculator/Calculator.java
@@ -1,15 +1,79 @@
 package entity.calculator;
 
+import entity.calculator.mahjong.MahjongGroup;
+import entity.calculator.mahjong.MahjongSuit;
+import entity.calculator.mahjong.MahjongTile;
 import entity.calculator.yaku.IYakuTestCase;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Calculator {
     private static final List<IYakuTestCase> yakuList = new ArrayList<IYakuTestCase>();
 
+    /***
+     * Determine whether the three tiles can be grouped in ascending order.
+     * The three tiles are in ascending order.
+     * @param tile the tile to consider
+     * @param tiles the remaining tiles
+     * @return true if they can be grouped in ascending order
+     */
+    private static boolean canGroupChii(MahjongTile tile, List<MahjongTile> tiles) {
+        if (!tile.getSuit().equals(MahjongSuit.DRAGON) && !tile.getSuit().equals(MahjongSuit.WIND)) {
+            if (tile.getValue() != 8 && tile.getValue() != 9) {
+                if (tileListIndex(tile.getValue()+1, tile.getSuit(), tiles) != -1 &&
+                        tileListIndex(tile.getValue()+2, tile.getSuit(), tiles) != -1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static int tileListIndex(int value, MahjongSuit suit, List<MahjongTile> tiles) {
+        for (int i=0; i<tiles.size(); i++) {
+            if (tiles.get(i).getValue() == value && tiles.get(i).getSuit().equals(suit)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private static boolean canGroupPon(MahjongTile tile, List<MahjongTile> tiles) {
+        //TODO: Implement canGroupPon
+        return true;
+    }
+
+    private static List<MahjongTile> orderTiles(List<MahjongTile> tiles) {
+        if (tiles == null) return new ArrayList<>();
+
+        List<MahjongTile> orderedTiles = new ArrayList<>(tiles);
+        orderedTiles.sort(Comparator.comparingInt(MahjongTile::getSuitOrder)
+                .thenComparingInt(MahjongTile::getRank));
+        return orderedTiles;
+    }
+
+    private static List<MahjongGroup> createGroups(HandState hand) {
+        List<MahjongGroup> groups = new ArrayList<>();
+        List<MahjongTile> tiles = orderTiles(hand.closedTiles());
+
+        if (tiles.size() < 3) {
+            return groups;
+        }
+
+        return groups;
+    }
+
     public static int calculateHan(HandState hand) {
-        return 0;
+        int han = 20;
+        //TODO: Check pinfu and chitoi case
+        if (hand.tsumo()) {
+            han += 2;
+        } else if (hand.ron() && hand.openGroups().isEmpty()) {
+            han += 10;
+        }
+        return han;
     }
 
     public static int calculateFu(HandState hand) {
--- src/main/java/entity/calculator/mahjong/MahjongTile.java
+++ src/main/java/entity/calculator/mahjong/MahjongTile.java
@@ -1,36 +1,36 @@
 package entity.calculator.mahjong;
 
 public enum MahjongTile {
-    ONE_MAN("1m", "tile/Man1.png", MahjongSuit.MAN, 1),
-    TWO_MAN("2m", "tile/Man2.png", MahjongSuit.MAN, 2),
-    THREE_MAN("3m", "tile/Man3.png", MahjongSuit.MAN, 3),
-    FOUR_MAN("4m", "tile/Man4.png", MahjongSuit.MAN, 4),
-    FIVE_MAN("5m", "tile/Man5.png", MahjongSuit.MAN, 5),
-    RED_FIVE_MAN("rm", "tile/Man5-Dora.png", MahjongSuit.MAN, 5, true),
-    SIX_MAN("6m", "tile/Man6.png", MahjongSuit.MAN, 6),
-    SEVEN_MAN("7m", "tile/Man7.png", MahjongSuit.MAN, 7),
-    EIGHT_MAN("8m", "tile/Man8.png", MahjongSuit.MAN, 8),
-    NINE_MAN("9m", "tile/Man9.png", MahjongSuit.MAN, 9),
-    ONE_SOU("1s", "tile/Sou1.png", MahjongSuit.SOU, 1),
-    TWO_SOU("2s", "tile/Sou2.png", MahjongSuit.SOU, 2),
-    THREE_SOU("3s", "tile/Sou3.png", MahjongSuit.SOU, 3),
-    FOUR_SOU("4s", "tile/Sou4.png", MahjongSuit.SOU, 4),
-    FIVE_SOU("5s", "tile/Sou5.png", MahjongSuit.SOU, 5),
-    RED_FIVE_SOU("rs", "tile/Sou5-Dora.png", MahjongSuit.SOU, 5, true),
-    SIX_SOU("6s", "tile/Sou6.png", MahjongSuit.SOU, 6),
-    SEVEN_SOU("7s", "tile/Sou7.png", MahjongSuit.SOU, 7),
-    EIGHT_SOU("8s", "tile/Sou8.png", MahjongSuit.SOU, 8),
-    NINE_SOU("9s", "tile/Sou9.png", MahjongSuit.SOU, 9),
-    ONE_PIN("1p", "tile/Pin1.png", MahjongSuit.PIN, 1),
-    TWO_PIN("2p", "tile/Pin2.png", MahjongSuit.PIN, 2),
-    THREE_PIN("3p", "tile/Pin3.png", MahjongSuit.PIN, 3),
-    FOUR_PIN("4p", "tile/Pin4.png", MahjongSuit.PIN, 4),
-    FIVE_PIN("5p", "tile/Pin5.png", MahjongSuit.PIN, 5),
-    RED_FIVE_PIN("rp", "tile/Pin5-Dora.png", MahjongSuit.PIN, 5, true),
-    SIX_PIN("6p", "tile/Pin6.png", MahjongSuit.PIN, 6),
-    SEVEN_PIN("7p", "tile/Pin7.png", MahjongSuit.PIN, 7),
-    EIGHT_PIN("8p", "tile/Pin8.png", MahjongSuit.PIN, 8),
-    NINE_PIN("9p", "tile/Pin9.png", MahjongSuit.PIN, 9),
+    ONE_MAN("1m", "tile/Man1.png", MahjongSuit.MAN, 1, 1),
+    TWO_MAN("2m", "tile/Man2.png", MahjongSuit.MAN, 2, 1),
+    THREE_MAN("3m", "tile/Man3.png", MahjongSuit.MAN, 3, 1),
+    FOUR_MAN("4m", "tile/Man4.png", MahjongSuit.MAN, 4, 1),
+    FIVE_MAN("5m", "tile/Man5.png", MahjongSuit.MAN, 5, 1),
+    RED_FIVE_MAN("rm", "tile/Man5-Dora.png", MahjongSuit.MAN, 5, 1, true),
+    SIX_MAN("6m", "tile/Man6.png", MahjongSuit.MAN, 6, 1),
+    SEVEN_MAN("7m", "tile/Man7.png", MahjongSuit.MAN, 7, 1),
+    EIGHT_MAN("8m", "tile/Man8.png", MahjongSuit.MAN, 8, 1),
+    NINE_MAN("9m", "tile/Man9.png", MahjongSuit.MAN, 9, 1),
+    ONE_SOU("1s", "tile/Sou1.png", MahjongSuit.SOU, 1, 2),
+    TWO_SOU("2s", "tile/Sou2.png", MahjongSuit.SOU, 2, 2),
+    THREE_SOU("3s", "tile/Sou3.png", MahjongSuit.SOU, 3, 2),
+    FOUR_SOU("4s", "tile/Sou4.png", MahjongSuit.SOU, 4, 2),
+    FIVE_SOU("5s", "tile/Sou5.png", MahjongSuit.SOU, 5, 2),
+    RED_FIVE_SOU("rs", "tile/Sou5-Dora.png", MahjongSuit.SOU, 5, 2, true),
+    SIX_SOU("6s", "tile/Sou6.png", MahjongSuit.SOU, 6, 2),
+    SEVEN_SOU("7s", "tile/Sou7.png", MahjongSuit.SOU, 7, 2),
+    EIGHT_SOU("8s", "tile/Sou8.png", MahjongSuit.SOU, 8, 2),
+    NINE_SOU("9s", "tile/Sou9.png", MahjongSuit.SOU, 9, 2),
+    ONE_PIN("1p", "tile/Pin1.png", MahjongSuit.PIN, 1, 3),
+    TWO_PIN("2p", "tile/Pin2.png", MahjongSuit.PIN, 2, 3),
+    THREE_PIN("3p", "tile/Pin3.png", MahjongSuit.PIN, 3, 3),
+    FOUR_PIN("4p", "tile/Pin4.png", MahjongSuit.PIN, 4, 3),
+    FIVE_PIN("5p", "tile/Pin5.png", MahjongSuit.PIN, 5, 3),
+    RED_FIVE_PIN("rp", "tile/Pin5-Dora.png", MahjongSuit.PIN, 5, 3, true),
+    SIX_PIN("6p", "tile/Pin6.png", MahjongSuit.PIN, 6, 3),
+    SEVEN_PIN("7p", "tile/Pin7.png", MahjongSuit.PIN, 7, 3),
+    EIGHT_PIN("8p", "tile/Pin8.png", MahjongSuit.PIN, 8, 3),
+    NINE_PIN("9p", "tile/Pin9.png", MahjongSuit.PIN, 9, 3),
     GREEN_DRAGON("gd", "tile/Hatsu.png", MahjongSuit.DRAGON),
     RED_DRAGON("rd", "tile/Chun.png", MahjongSuit.DRAGON),
     WHITE_DRAGON("wd", "tile/Haku.png", MahjongSuit.DRAGON),
@@ -45,18 +45,20 @@ public enum MahjongTile {
     private final boolean terminal;
     private final MahjongSuit suit;
     private final boolean aka;
+    private final int rank;
 
     MahjongTile(String serialize, String filePath, MahjongSuit suit) {
-        this(serialize, filePath, suit, -1, false);
+        this(serialize, filePath, suit, -1, -1, false);
     }
 
-    MahjongTile(String serialize, String filePath, MahjongSuit suit, int value) {
-        this(serialize, filePath, suit, value, false);
+    MahjongTile(String serialize, String filePath, MahjongSuit suit, int value, int rank) {
+        this(serialize, filePath, suit, rank, value, false);
     }
 
-    MahjongTile(String serialize, String filePath, MahjongSuit suit, int value, boolean aka) {
+    MahjongTile(String serialize, String filePath, MahjongSuit suit, int value, int rank, boolean aka) {
         this.serialize = serialize;
         this.filePath = filePath;
+        this.rank = rank;
         this.value = value;
         this.suit = suit;
         this.terminal = value == 1 || value == 9;
@@ -87,6 +89,8 @@ public enum MahjongTile {
         return aka;
     }
 
+    public int getRank() { return rank; }
+
     public static MahjongTile getMahjongTile(String serialize) {
         for (MahjongTile tile : MahjongTile.values()) {
             if (tile.getSerialization().equals(serialize)) {
@@ -95,5 +99,16 @@ public enum MahjongTile {
         }
 
         return null;
+    }
+
+    public int getSuitOrder() {
+        return switch (suit) {
+            case MAN -> 1;
+            case SOU -> 2;
+            case PIN -> 3;
+            case DRAGON -> 4;
+            case WIND -> 5;
+            default -> Integer.MAX_VALUE;
+        };
     }
 }
