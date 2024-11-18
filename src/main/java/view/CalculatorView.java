a404483afeb7930c3fa13bf1144b063f1715da01
david-703 <jsdavid.won@gmail.com>
2024-11-18 16:31:54 -0500

    Removed unnecessary import statements.

--- src/main/java/view/CalculatorView.java
+++ src/main/java/view/CalculatorView.java
@@ -1,17 +1,11 @@
 package view;
 
-import app.IApp;
-import entity.calculator.mahjong.MahjongGroup;
-import entity.calculator.mahjong.MahjongTile;
 import interface_adapter.ViewManager;
-import interface_adapter.ViewState;
 import interface_adapter.calculator.CalculatorState;
 import interface_adapter.calculator.CalculatorViewState;
-import view.component.ITileSelectorMaster;
 import view.component.DisplayHandComponent;
 import view.component.TileSelectorComponent;
 
-import javax.swing.*;
 import java.awt.*;
 
 public class CalculatorView extends AbstractPanel<CalculatorState> {
@@ -27,7 +21,7 @@ public class CalculatorView extends AbstractPanel<CalculatorState> {
         add(displayHandComponent, BorderLayout.NORTH);
 
         // Initialize and add TileSelectorComponent at center
-        tileSelectorComponent = new TileSelectorComponent(this);
+        tileSelectorComponent = new TileSelectorComponent(viewState);
         add(tileSelectorComponent, BorderLayout.CENTER);
     }
 
