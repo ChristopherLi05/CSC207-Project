d2fdb68a93fbb51b56c9d2bda91d6f3738bf58d8
Hiro2974 <hiro.yano@utoronto.ca.mail>
2024-11-18 16:54:58 -0500

    wrote trainercontroller

--- src/main/java/interface_adapter/Trainer/TrainerController.java
+++ src/main/java/interface_adapter/Trainer/TrainerController.java
@@ -1,4 +1,19 @@
 package interface_adapter.Trainer;
 
+import use_case.trainer.TrainerInputBoundry;
+import use_case.trainer.TrainerInputData;
+
 public class TrainerController {
+    private final TrainerInputBoundry trainerUseCaseInteractor;
+
+    public TrainerController(TrainerInputBoundry trainerUseCaseInteractor) {
+        this.trainerUseCaseInteractor = trainerUseCaseInteractor;
+    }
+
+
+    public void execute(long attempt) {
+        final TrainerInputData trainerInputData = new TrainerInputData(attempt);
+
+        trainerUseCaseInteractor.execute(trainerInputData);
+    }
 }
