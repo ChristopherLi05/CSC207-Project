cec7e1e6f58b0844a1f3d31d16043a95557d18f7
MaxChen102 <maxx.chen@mail.utoronto.ca>
2024-11-18 16:46:57 -0500

    "added log in interactor"

--- src/main/java/use_case/login/LoginInteractor.java
+++ src/main/java/use_case/login/LoginInteractor.java
@@ -1,4 +1,26 @@
 package use_case.login;
 
+import data_access.IDataAccessor;
+
 public class LoginInteractor implements LoginInputBoundary {
+    private final IDataAccessor dataAccessor;
+    private final LoginOutputBoundary loginPresenter;
+
+    public LoginInteractor(IDataAccessor dataAccessor,
+                           LoginOutputBoundary loginPresenter) {
+        this.dataAccessor = dataAccessor;
+        this.loginPresenter = loginPresenter;
+    }
+
+    public void execute(LoginInputData loginInputData) {
+        final String username = loginInputData.getUsername();
+        final String password = loginInputData.getPassword();
+        final String result = dataAccessor.logIn(username, password);
+
+        if (!result.equals(username)) {
+            loginPresenter.prepareFailView();
+        } else {
+
+        }
+    }
 }
