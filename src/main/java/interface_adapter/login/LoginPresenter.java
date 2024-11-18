60cc756488cccd5ccb142f6a3e8a5e1b9c6311e0
MaxChen102 <maxx.chen@mail.utoronto.ca>
2024-11-18 16:46:20 -0500

    "added log in presenter"

--- src/main/java/interface_adapter/login/LoginPresenter.java
+++ src/main/java/interface_adapter/login/LoginPresenter.java
@@ -1,6 +1,34 @@
 package interface_adapter.login;
 
 import use_case.login.LoginOutputBoundary;
+import use_case.login.LoginOutputData;
 
 public class LoginPresenter implements LoginOutputBoundary {
+
+    private final LoginViewState loginViewState;
+    private final SignupViewState signupViewState;
+    private final CalculatorViewState calculatorViewState;
+
+    public LoginPresenter(LoginViewState loginViewState,
+                          SignupViewState signupViewState,
+                          CalculatorViewState calculatorViewState) {
+        this.loginViewState = loginViewState;
+        this.signupViewState = signupViewState;
+        this.calculatorViewState = calculatorViewState;
+    }
+
+    @Override
+    public void prepareSignupView(LoginOutputData loginOutputData) {
+
+    }
+
+    @Override
+    public void prepareCalculatorView(LoginOutputData loginOutputData) {
+
+    }
+
+    @Override
+    public void prepareFailView(String message) {
+
+    }
 }
--- src/main/java/use_case/login/LoginOutputBoundary.java
+++ src/main/java/use_case/login/LoginOutputBoundary.java
@@ -1,4 +1,7 @@
 package use_case.login;
 
 public interface LoginOutputBoundary {
+    void prepareSignupView(LoginOutputData loginOutputData);
+    void prepareCalculatorView(LoginOutputData loginOutputData);
+    void prepareFailView(String error);
 }
