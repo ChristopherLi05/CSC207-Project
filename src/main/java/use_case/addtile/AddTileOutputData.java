d28f1c93e5b87b5182a49de1ac37e729babd45fd
david-703 <jsdavid.won@gmail.com>
2024-11-21 16:08:31 -0500

    Implemented AddTileOutputdata.

--- src/main/java/use_case/addtile/AddTileOutputData.java
+++ src/main/java/use_case/addtile/AddTileOutputData.java
@@ -1,15 +1,22 @@
 package use_case.addtile;
 
 import entity.calculator.mahjong.MahjongGroup;
+import entity.calculator.mahjong.MahjongTile;
 
 public class AddTileOutputData {
     private String type = null;
     private Object newTiles = null;
 
     public AddTileOutputData(Object newTiles) {
+        this.newTiles = newTiles;
         if (newTiles instanceof MahjongGroup) {
             this.type = "group";
-            this.newTiles = newTiles;
+        }
+        else if (newTiles instanceof MahjongTile) {
+            this.type = "tile";
+        }
+        else {
+            this.type = "none";
         }
     }
 }
