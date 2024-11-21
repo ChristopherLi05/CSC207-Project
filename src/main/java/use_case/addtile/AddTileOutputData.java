a186277fb9548712fdc735dbf13cdcb6b74a5f0c
david-703 <jsdavid.won@gmail.com>
2024-11-21 15:51:42 -0500

    Incomplete implementation of AddTileOutputData.

--- src/main/java/use_case/addtile/AddTileOutputData.java
+++ src/main/java/use_case/addtile/AddTileOutputData.java
@@ -3,11 +3,13 @@ package use_case.addtile;
 import entity.calculator.mahjong.MahjongGroup;
 
 public class AddTileOutputData {
-    public Object newTiles;
+    private String type = null;
+    private Object newTiles = null;
 
     public AddTileOutputData(Object newTiles) {
         if (newTiles instanceof MahjongGroup) {
-
+            this.type = "group";
+            this.newTiles = newTiles;
         }
     }
 }
