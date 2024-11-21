befdf723ebc969987bef345646442afdaeeea8a1
MaxChen102 <maxx.chen@mail.utoronto.ca>
2024-11-21 01:19:02 -0500

    "revert"

--- src/main/java/use_case/tabswitcher/TabSwitcherInputBoundary.java
+++ src/main/java/use_case/tabswitcher/TabSwitcherInputBoundary.java
@@ -1,7 +1,4 @@
 package use_case.tabswitcher;
 
 public interface TabSwitcherInputBoundary {
-    void calculator();
-    void trainer();
-    void puzzlerush();
 }
--- src/main/java/use_case/tabswitcher/TabSwitcherOutputBoundary.java
+++ src/main/java/use_case/tabswitcher/TabSwitcherOutputBoundary.java
@@ -1,7 +1,4 @@
 package use_case.tabswitcher;
 
 public interface TabSwitcherOutputBoundary {
-    void prepareCalculatorView();
-    void prepareTrainerView();
-    void preparePuzzleRushView();
 }
