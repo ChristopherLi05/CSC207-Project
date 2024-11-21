0b1bccd1b11273644d284f7303111e14897bf438
Christopher Li <@>
2024-11-21 00:50:15 -0500

    add basic impl so program doesn't error out

--- src/main/java/view/LoginView.java
+++ src/main/java/view/LoginView.java
@@ -1,9 +1,10 @@
 package view;
 
 import app.IApp;
+import interface_adapter.ViewState;
 
-public class LoginView extends AbstractPanel<> {
-    public LoginView(IApp master) {
-        super(master);
+public class LoginView extends AbstractPanel<Object> {
+    public LoginView(ViewState<Object> viewState) {
+        super(viewState);
     }
 }
