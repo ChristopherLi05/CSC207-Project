114a70deae30e8a8313abd8daad7dddb075d0ee7
david-703 <jsdavid.won@gmail.com>
2024-11-21 14:07:13 -0500

    Implemented AddTileInputData.

--- src/main/java/use_case/addTile/AddTileInputData.java
+++ src/main/java/use_case/addTile/AddTileInputData.java
@@ -1,5 +1,17 @@
 package use_case.addTile;
 
-public class AddTileInputData {
+import view.component.ITileSelectorMaster;
+import view.component.MahjongTileInputButton;
 
+public class AddTileInputData {
+    private final MahjongTileInputButton tileClicked;
+    private final boolean isAka;
+    private final ITileSelectorMaster.SelectorType selectorType;
+
+    public AddTileInputData(Object tileClicked, boolean isAka, ITileSelectorMaster.SelectorType selectorType) {
+        this.tileClicked = (MahjongTileInputButton) tileClicked;
+        this.isAka = isAka;
+        this.selectorType = selectorType;
+
+    }
 }
