5aab172204f5b510750b95b68cd95abc30d46df0
david-703 <jsdavid.won@gmail.com>
2024-11-21 13:53:48 -0500

    Added instance variables and constructor for AddTilecontroller.

--- src/main/java/interface_adapter/addTile/AddTileController.java
+++ src/main/java/interface_adapter/addTile/AddTileController.java
@@ -1,4 +1,15 @@
 package interface_adapter.addTile;
 
+import use_case.addTile.AddTileInputBoundary;
+import use_case.addTile.AddTileInputData;
+
 public class AddTileController {
+    private final AddTileInputBoundary addTileInput;
+    private final AddTileViewState addTileViewState;
+
+
+    public AddTileController(AddTileInputBoundary addTileInput, AddTileViewState addTileViewState) {
+        this.addTileInput = addTileInput;
+        this.addTileViewState = addTileViewState;
+    }
 }
