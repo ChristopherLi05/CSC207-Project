57233faa6d00a1ae2c6af31b71de69f134fd3c66
david-703 <jsdavid.won@gmail.com>
2024-11-21 14:08:19 -0500

    Added an extra comment in the execute method of AddTileController.

--- src/main/java/interface_adapter/addTile/AddTileController.java
+++ src/main/java/interface_adapter/addTile/AddTileController.java
@@ -2,6 +2,7 @@ package interface_adapter.addTile;
 
 import use_case.addTile.AddTileInputBoundary;
 import use_case.addTile.AddTileInputData;
+import view.component.ITileSelectorMaster;
 
 public class AddTileController {
     private final AddTileInputBoundary addTileInput;
@@ -11,5 +12,13 @@ public class AddTileController {
     public AddTileController(AddTileInputBoundary addTileInput, AddTileViewState addTileViewState) {
         this.addTileInput = addTileInput;
         this.addTileViewState = addTileViewState;
+    }
+
+    public void execute(Object tileClicked, boolean isAka, ITileSelectorMaster.SelectorType selectorType) {
+        // Create input data
+        AddTileInputData inputData = new AddTileInputData(tileClicked, isAka, selectorType);
+
+        // Pass input data
+        addTileInput.execute(inputData);
     }
 }
