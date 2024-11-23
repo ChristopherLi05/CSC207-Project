236b5bd445af2ef672ce249c1254e947fa598ad9
Hiro2974 <hiro.yano@utoronto.ca.mail>
2024-11-23 01:50:23 -0500

    added a button to calculate the score. Also chnaged the input into the selectedtiles from displaycomponent

--- src/main/java/view/CalculatorView.java
+++ src/main/java/view/CalculatorView.java
@@ -1,11 +1,15 @@
 package view;
 
 import interface_adapter.ViewManager;
+import interface_adapter.calculator.CalculatorController;
 import interface_adapter.calculator.CalculatorState;
 import interface_adapter.calculator.CalculatorViewState;
+
+import interface_adapter.login.LoginController;
 import view.component.DisplayHandComponent;
 import view.component.TileSelectorComponent;
 
+import javax.swing.*;
 import java.awt.*;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
@@ -13,6 +17,8 @@ import java.beans.PropertyChangeListener;
 public class CalculatorView extends AbstractPanel<CalculatorState> implements PropertyChangeListener {
     private final TileSelectorComponent tileSelectorComponent;
     private final DisplayHandComponent displayHandComponent;
+    private CalculatorController calculatorController;
+    private final JButton calculate;
 
     public CalculatorView(CalculatorViewState viewState, ViewManager viewManager) {
         super(viewState);
@@ -28,14 +34,21 @@ public class CalculatorView extends AbstractPanel<CalculatorState> implements Pr
 
         viewState.addPropertyChangeListener(this);
         viewManager.addPropertyChangeListener(this);
+
+        final JPanel buttons = new JPanel();
+        calculate = new JButton("calculate");
+        buttons.add(calculate);
+
+        calculate.addActionListener(evt -> calculatorController.execute(displayHandComponent.getSelectedTiles()));
     }
 
     public DisplayHandComponent getDisplayHandComponent() {
         return displayHandComponent;
     }
 
-    @Override
-    public void propertyChange(PropertyChangeEvent evt) {
-
+    public void setCalculatorController(CalculatorController calculatorController) {
+        this.calculatorController = calculatorController;
     }
+
 }
+
