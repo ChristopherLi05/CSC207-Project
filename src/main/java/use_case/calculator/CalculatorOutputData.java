d94d56b314407fa7ca20b2a00e923bc40ab8d84c
Hiro2974 <hiro.yano@utoronto.ca.mail>
2024-11-23 01:43:35 -0500

    updated calculatoroutputdata

--- src/main/java/use_case/calculator/CalculatorOutputData.java
+++ src/main/java/use_case/calculator/CalculatorOutputData.java
@@ -2,5 +2,17 @@ package use_case.calculator;
 
 import entity.calculator.HandState;
 
-public record CalculatorOutputData(int score, boolean useCaseFailed) {
+public class CalculatorOutputData {
+
+    private final int score;
+    private final boolean useCaseFailed;
+
+    public CalculatorOutputData(int score, boolean useCaseFailed) {
+        this.score = score;
+        this.useCaseFailed = useCaseFailed;
+    }
+
+    public int getScore() {
+        return score;
+    }
 }
