1791dce892a6bc620700e554ef36e32919c7a04b
Hiro2974 <hiro.yano@utoronto.ca.mail>
2024-11-23 01:46:53 -0500

    need to be updated with input for calculatorstate

--- src/main/java/interface_adapter/calculator/CalculatorPresenter.java
+++ src/main/java/interface_adapter/calculator/CalculatorPresenter.java
@@ -1,4 +1,24 @@
 package interface_adapter.calculator;
 
-public class CalculatorPresenter {
+import use_case.calculator.CalculatorOutputBoundary;
+import use_case.calculator.CalculatorOutputData;
+
+
+public class CalculatorPresenter implements CalculatorOutputBoundary {
+    private final CalculatorViewState calculatorViewState;
+
+    public CalculatorPresenter(CalculatorViewState calculatorViewState) {
+        this.calculatorViewState = calculatorViewState;
+    }
+
+    @Override
+    public void prepareSuccessView(CalculatorOutputData outputData) {
+        calculatorViewState.setState(new CalculatorState());
+        calculatorViewState.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareFailView(String errorMessage) {
+        // note: this use case currently can't fail
+    }
 }
