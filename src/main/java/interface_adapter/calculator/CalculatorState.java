3972d85b137260d15cd77b9e35ab0ddd6c619ce2
Hiro2974 <hiro.yano@utoronto.ca.mail>
2024-11-23 01:47:08 -0500

    need to be updated with input for calculatorstate

--- src/main/java/interface_adapter/calculator/CalculatorState.java
+++ src/main/java/interface_adapter/calculator/CalculatorState.java
@@ -3,59 +3,89 @@ package interface_adapter.calculator;
 import entity.calculator.Calculator;
 import entity.calculator.mahjong.MahjongGroup;
 import entity.calculator.mahjong.MahjongTile;
+import use_case.calculator.CalculatorDataAccessInterface;
 import view.component.ITileSelectorMaster;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class CalculatorState implements ITileSelectorMaster {
-    private final List<MahjongTile> selectedTiles = new ArrayList<>();
-    private boolean containsAka = false;
-    private boolean isOpen = false;
+public class CalculatorState implements CalculatorDataAccessInterface {
+    private final List<MahjongTile> closedTiles;
+    private final List<MahjongGroup> closedGroup;
+    private final List<MahjongGroup> openedGroup;
+    private final MahjongTile winningTile;
 
-    public List<MahjongTile> getSelectedTiles() {
-        return selectedTiles;
+    public CalculatorState(List<MahjongTile> closedTiles, List<MahjongGroup> closedGroup, List<MahjongGroup> openedGroup, MahjongTile winningTile) {
+        this.closedTiles = closedTiles;
+        this.closedGroup = closedGroup;
+        this.openedGroup = openedGroup;
+        this.winningTile = winningTile;
     }
 
-    public boolean isContainsAka() {
-        return containsAka;
-    }
 
-    public void setContainsAka(boolean containsAka) {
-        this.containsAka = containsAka;
-    }
+    String getClosedTiles() {};
 
-    public boolean isOpen() {
-        return isOpen;
-    }
+    void addClosedTiles(List<MahjongTile> closedTiles);
 
-    public void setIsOpen(boolean isOpen) {
-        this.isOpen = isOpen;
-    }
+    String getClosedGroup();
 
-    @Override
-    public void addClosedTile(MahjongTile mahjongTile) {
-        System.out.println("Closed tile: " + mahjongTile);
-    }
+    void addClosedGroup(List<MahjongGroup> closedGroup);
 
-    @Override
-    public void addChiiGroup(MahjongGroup mahjongGroup) {
-        System.out.println("Chii Group: " + mahjongGroup);
-    }
+    String getOpenGroups();
 
-    @Override
-    public void addPonGroup(MahjongGroup mahjongGroup) {
-        System.out.println("Chii Group: " + mahjongGroup);
-    }
+    void addOpenGroups(List<MahjongGroup> openGroups);
 
-    @Override
-    public void addClosedKanGroup(MahjongGroup mahjongGroup) {
-        System.out.println("Closed Kan Group: " + mahjongGroup);
-    }
+    String getWinningTile();
 
-    @Override
-    public void addOpenKanGroup(MahjongGroup mahjongGroup) {
-        System.out.println("Open Kan Group: " + mahjongGroup);
-    }
+    void addWinningTile(MahjongTile winningTile);
+
+//    private final List<MahjongTile> selectedTiles = new ArrayList<>();
+//    private boolean containsAka = false;
+//    private boolean isOpen = false;
+//
+//    public List<MahjongTile> getSelectedTiles() {
+//        return selectedTiles;
+//    }
+//
+//    public boolean isContainsAka() {
+//        return containsAka;
+//    }
+//
+//    public void setContainsAka(boolean containsAka) {
+//        this.containsAka = containsAka;
+//    }
+//
+//    public boolean isOpen() {
+//        return isOpen;
+//    }
+//
+//    public void setIsOpen(boolean isOpen) {
+//        this.isOpen = isOpen;
+//    }
+//
+//    @Override
+//    public void addClosedTile(MahjongTile mahjongTile) {
+//        System.out.println("Closed tile: " + mahjongTile);
+//    }
+//
+//    @Override
+//    public void addChiiGroup(MahjongGroup mahjongGroup) {
+//        System.out.println("Chii Group: " + mahjongGroup);
+//    }
+//
+//    @Override
+//    public void addPonGroup(MahjongGroup mahjongGroup) {
+//        System.out.println("Chii Group: " + mahjongGroup);
+//    }
+//
+//    @Override
+//    public void addClosedKanGroup(MahjongGroup mahjongGroup) {
+//        System.out.println("Closed Kan Group: " + mahjongGroup);
+//    }
+//
+//    @Override
+//    public void addOpenKanGroup(MahjongGroup mahjongGroup) {
+//        System.out.println("Open Kan Group: " + mahjongGroup);
+//        }
 }
 
