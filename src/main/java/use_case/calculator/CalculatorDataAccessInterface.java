7861ac507fe3179544f01d069e567ddefab41eef
Hiro2974 <hiro.yano@utoronto.ca.mail>
2024-11-23 01:51:31 -0500

    Made a new dataaccessinterface because itileselectormastr is used for tileselectorcomponent

--- src/main/java/use_case/calculator/CalculatorDataAccessInterface.java
+++ src/main/java/use_case/calculator/CalculatorDataAccessInterface.java
@@ -1,4 +1,24 @@
 package use_case.calculator;
 
+import entity.calculator.mahjong.MahjongGroup;
+import entity.calculator.mahjong.MahjongTile;
+
+import java.util.List;
+
 public interface CalculatorDataAccessInterface {
+    String getClosedTiles();
+
+    void addClosedTiles(List<MahjongTile> closedTiles);
+
+    String getClosedGroup();
+
+    void addClosedGroup(List<MahjongGroup> closedGroup);
+
+    String getOpenGroups();
+
+    void addOpenGroups(List<MahjongGroup> openGroups);
+
+    String getWinningTile();
+
+    void addWinningTile(MahjongTile winningTile);
 }
