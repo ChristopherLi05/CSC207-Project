c7c38647485c278b6ac550e347fef510154988a0
Hiro2974 <hiro.yano@utoronto.ca.mail>
2024-11-23 01:42:46 -0500

    updated calculatorController so it takes selctedtiles as input

--- src/main/java/interface_adapter/calculator/CalculatorController.java
+++ src/main/java/interface_adapter/calculator/CalculatorController.java
@@ -1,4 +1,23 @@
 package interface_adapter.calculator;
 
+import entity.calculator.HandState;
+import entity.calculator.mahjong.MahjongTile;
+import use_case.calculator.CalculatorInputBoundary;
+import use_case.calculator.CalculatorInputData;
+
+import java.util.List;
+
 public class CalculatorController {
+    private final CalculatorInputBoundary calculatorUseCaseInteractor;
+
+    public CalculatorController(CalculatorInputBoundary calculatorUseCaseInteractor) {
+        this.calculatorUseCaseInteractor = calculatorUseCaseInteractor;
+    }
+
+
+    public void execute(List<MahjongTile> selectedtiles) {
+        final CalculatorInputData calculatorInputData = new CalculatorInputData(selectedtiles);
+
+        calculatorUseCaseInteractor.execute(calculatorInputData);
+    }
 }
