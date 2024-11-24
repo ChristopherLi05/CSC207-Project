53044d2a88ae3241952237a0114a04a728dd1aa9
ChristopherLi05 <admissions.christopher@gmail.com>
2024-11-23 19:10:33 -0500

    reformat

--- src/main/java/use_case/addTile/AddTileInputData.java
+++ src/main/java/use_case/addTile/AddTileInputData.java
@@ -1,23 +1,28 @@
 package use_case.addTile;
 
 import entity.calculator.mahjong.MahjongTile;
-import view.component.TileSelectorComponentState;
+import view.component.ITileSelectorComponentState;
 
 public class AddTileInputData {
     private final MahjongTile tile;
     private final boolean isAka;
-    private final TileSelectorComponentState.SelectorType selectorType;
+    private final ITileSelectorComponentState.SelectorType selectorType;
 
-    public AddTileInputData(MahjongTile tile, boolean isAka, TileSelectorComponentState.SelectorType selectorType) {
+    public AddTileInputData(MahjongTile tile, boolean isAka, ITileSelectorComponentState.SelectorType selectorType) {
         this.tile = tile;
         this.isAka = isAka;
         this.selectorType = selectorType;
-
     }
 
-    public TileSelectorComponentState.SelectorType getSelectorType() {return selectorType;}
+    public ITileSelectorComponentState.SelectorType getSelectorType() {
+        return selectorType;
+    }
 
-    public MahjongTile getTile() {return tile;}
+    public MahjongTile getTile() {
+        return tile;
+    }
 
-    public boolean isAka() {return isAka;}
+    public boolean isAka() {
+        return isAka;
+    }
 }
