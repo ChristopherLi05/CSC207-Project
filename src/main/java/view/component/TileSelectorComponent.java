c2a3bc2a65b69b3a32e9a4d5766c2b30be3b83d6
ChristopherLi05 <admissions.christopher@gmail.com>
2024-11-23 19:02:57 -0500

    fix tile selector component

--- src/main/java/view/component/TileSelectorComponent.java
+++ src/main/java/view/component/TileSelectorComponent.java
@@ -6,31 +6,28 @@ import interface_adapter.calculator.CalculatorViewState;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.ActionListener;
 
 public class TileSelectorComponent extends JPanel {
     private final Color UNTOGGLE_COLOR = new Color(0xFFFFFF);
     private final Color TOGGLE_COLOR = new Color(0x89CFF0);
 
-    private final ITileSelectorComponentState master;
-
-    private boolean containsAka = false;
-    private ITileSelectorComponentState.SelectorType selectorType = ITileSelectorComponentState.SelectorType.NONE;
+    private final CalculatorViewState calculatorViewState;
 
     private final JButton chiiButton;
     private final JButton ponButton;
     private final JButton closedKanButton;
     private final JButton openKanButton;
 
-    public TileSelectorComponent(CalculatorViewState calculatorViewState) {
-        this.master = null;
+    public TileSelectorComponent(CalculatorViewState calculatorViewState, ActionListener tileListener) {
+        this.calculatorViewState = calculatorViewState;
         setLayout(new BorderLayout());
 
         // Control panel with action type buttons and checkboxes
         JPanel controlPanel = new JPanel(new FlowLayout());
 
-        // Checkboxes
         JCheckBox containsAkaCheckbox = new JCheckBox("Contains Aka");
-        containsAkaCheckbox.addActionListener(e -> containsAka = containsAkaCheckbox.isSelected());
+        containsAkaCheckbox.addActionListener(e -> calculatorViewState.getState().setAka(containsAkaCheckbox.isSelected()));
         controlPanel.add(containsAkaCheckbox);
 
         // Action type buttons
@@ -64,7 +61,8 @@ public class TileSelectorComponent extends JPanel {
 
         // Initialize buttons for each MahjongTile in enum
         for (MahjongTile tile : MahjongTile.values()) {
-            MahjongTileInputButton button = getMahjongTileInputButton(tile);
+            MahjongTileInputButton button = new MahjongTileInputButton(tile);
+            button.addActionListener(tileListener);
             tilePanel.add(button);
         }
 
@@ -76,41 +74,14 @@ public class TileSelectorComponent extends JPanel {
         setBorder(new EmptyBorder(10, 10, 10, 10));
     }
 
-    private MahjongTileInputButton getMahjongTileInputButton(MahjongTile tile) {
-        MahjongTileInputButton button = new MahjongTileInputButton(tile);
-        button.addActionListener(e -> {
-            // Send to use case interactor
-            if (e.getSource() instanceof MahjongTileInputButton) {
-                MahjongTile tile1 = ((MahjongTileInputButton) e.getSource()).getMahjongTile();
+    public void toggleSelectorType(ITileSelectorComponentState.SelectorType newSelectorType) {
+        ITileSelectorComponentState.SelectorType oldSelectorType = calculatorViewState.getState().getSelectorType();
+        calculatorViewState.getState().setSelectorType(oldSelectorType == newSelectorType ? ITileSelectorComponentState.SelectorType.NONE : newSelectorType);
+        newSelectorType = calculatorViewState.getState().getSelectorType();
 
-                if (selectorType == ITileSelectorComponentState.SelectorType.NONE) {
-                    addClosedTile(tile1);
-                } else if (selectorType == ITileSelectorComponentState.SelectorType.CHII) {
-                    addChii(tile1);
-                } else if (selectorType == ITileSelectorComponentState.SelectorType.PON) {
-                    addPon(tile1);
-                } else if (selectorType == ITileSelectorComponentState.SelectorType.CLOSED_KAN) {
-                    addClosedKan(tile1);
-                } else if (selectorType == ITileSelectorComponentState.SelectorType.OPEN_KAN) {
-                    addOpenKan(tile1);
-                }
-            }
-        });
-        return button;
+        this.chiiButton.setBackground(newSelectorType == ITileSelectorComponentState.SelectorType.CHII ? TOGGLE_COLOR : UNTOGGLE_COLOR);
+        this.ponButton.setBackground(newSelectorType == ITileSelectorComponentState.SelectorType.PON ? TOGGLE_COLOR : UNTOGGLE_COLOR);
+        this.closedKanButton.setBackground(newSelectorType == ITileSelectorComponentState.SelectorType.CLOSED_KAN ? TOGGLE_COLOR : UNTOGGLE_COLOR);
+        this.openKanButton.setBackground(newSelectorType == ITileSelectorComponentState.SelectorType.OPEN_KAN ? TOGGLE_COLOR : UNTOGGLE_COLOR);
     }
-
-    public void toggleSelectorType(ITileSelectorComponentState.SelectorType selectorType) {
-        this.selectorType = selectorType == this.selectorType ? ITileSelectorComponentState.SelectorType.NONE : selectorType;
-
-        this.chiiButton.setBackground(this.selectorType == ITileSelectorComponentState.SelectorType.CHII ? TOGGLE_COLOR : UNTOGGLE_COLOR);
-        this.ponButton.setBackground(this.selectorType == ITileSelectorComponentState.SelectorType.PON ? TOGGLE_COLOR : UNTOGGLE_COLOR);
-        this.closedKanButton.setBackground(this.selectorType == ITileSelectorComponentState.SelectorType.CLOSED_KAN ? TOGGLE_COLOR : UNTOGGLE_COLOR);
-        this.openKanButton.setBackground(this.selectorType == ITileSelectorComponentState.SelectorType.OPEN_KAN ? TOGGLE_COLOR : UNTOGGLE_COLOR);
-    }
-
-    public ITileSelectorComponentState.SelectorType getSelectorType() {
-        return selectorType;
-    }
-
-    public boolean containsAka() {return containsAka;}
 }
