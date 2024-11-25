c8a0937d56903544e4f70f34b17bf4158ee890ce
Hiro2974 <hiro.yano@utoronto.ca.mail>
2024-11-25 14:56:13 -0500

    added meesage to the prepare views

--- src/main/java/interface_adapter/calculator/CalculatorPresenter.java
+++ src/main/java/interface_adapter/calculator/CalculatorPresenter.java
@@ -1,5 +1,6 @@
 package interface_adapter.calculator;
 
+import interface_adapter.login.LoginState;
 import use_case.calculator.CalculatorOutputBoundary;
 import use_case.calculator.CalculatorOutputData;
 
@@ -10,15 +11,18 @@ public class CalculatorPresenter implements CalculatorOutputBoundary {
     public CalculatorPresenter(CalculatorViewState calculatorViewState) {
         this.calculatorViewState = calculatorViewState;
     }
-
+//TODO make it present the message and score
     @Override
-    public void prepareSuccessView(CalculatorOutputData outputData) {
-        calculatorViewState.setState(new CalculatorState());
+    public void prepareSuccessView(String successMessage, CalculatorOutputData outputData) {
+        final CalculatorState calculatorState = calculatorViewState.getState();
+        calculatorState.setMessageState(successMessage);
         calculatorViewState.firePropertyChanged();
     }
-
+//TODO
     @Override
     public void prepareFailView(String errorMessage) {
-        // note: this use case currently can't fail
+        final CalculatorState calculatorState = calculatorViewState.getState();
+        calculatorState.setMessageState(errorMessage);
+        calculatorViewState.firePropertyChanged();
     }
 }
