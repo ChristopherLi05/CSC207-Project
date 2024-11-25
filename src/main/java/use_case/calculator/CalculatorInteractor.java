60d69f9d6253bd7b51e0cb0f8b528a0be9723b6c
Hiro2974 <hiro.yano@utoronto.ca.mail>
2024-11-25 16:37:56 -0500

    modified the condition so that there's no error

--- src/main/java/use_case/calculator/CalculatorInteractor.java
+++ src/main/java/use_case/calculator/CalculatorInteractor.java
@@ -2,17 +2,6 @@ package use_case.calculator;
 
 import entity.calculator.Calculator;
 import entity.calculator.HandState;
-import entity.calculator.HandStateFactory;
-import entity.calculator.IHandStateFactory;
-import entity.calculator.mahjong.MahjongGroup;
-import entity.calculator.mahjong.MahjongTile;
-import use_case.login.LoginOutputData;
-import view.component.IDisplayHandComponentState;
-import view.component.ITileSelectorMaster;
-
-import java.util.List;
-
-import static use_case.calculator.CalculatorDataAccessInterface.east;
 
 public class CalculatorInteractor implements CalculatorInputBoundary{
     private final CalculatorOutputBoundary calculatorPresenter;
@@ -24,8 +13,7 @@ public class CalculatorInteractor implements CalculatorInputBoundary{
     @Override
     public void execute(HandState hand) {
         int score = Calculator.calculateScore(hand);
-        //TODO if the handstate was not correct or sufficient
-        if (score == null) {
+        if (score == 0) {
             calculatorPresenter.prepareFailView("Invalid hand");
         } else {
             calculatorPresenter.prepareSuccessView("Score is", new CalculatorOutputData(score));
