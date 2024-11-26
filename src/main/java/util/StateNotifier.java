5efa9247fbb93929d8ca78558772795dd121dd44
Christopher Li <admissions.christopher@gmail.com>
2024-11-26 16:54:01 -0500

    javadocs

--- src/main/java/util/StateNotifier.java
+++ src/main/java/util/StateNotifier.java
@@ -3,6 +3,10 @@ package util;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
+/**
+ * State notifier class, was originally viewmodel but changed to this
+ * @param <T>
+ */
 public class StateNotifier<T> {
     private final PropertyChangeSupport support = new PropertyChangeSupport(this);
 
@@ -20,14 +24,25 @@ public class StateNotifier<T> {
         this.state = state;
     }
 
+    /**
+     * Notifies listeners that property was changed
+     */
     public void firePropertyChanged() {
         this.firePropertyChanged("state");
     }
 
+    /**
+     * Notifies listeners that property was changed
+     * @param propertyName custom name
+     */
     public void firePropertyChanged(String propertyName) {
         this.support.firePropertyChange(propertyName, null, this.state);
     }
 
+    /**
+     * Adds property change listener to observers
+     * @param listener listener
+     */
     public void addPropertyChangeListener(PropertyChangeListener listener) {
         this.support.addPropertyChangeListener(listener);
     }
