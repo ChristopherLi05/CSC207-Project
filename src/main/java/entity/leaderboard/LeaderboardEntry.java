95750ee1a4257391438537e38a17c9803211d524
Christopher Li <admissions.christopher@gmail.com>
2024-11-26 16:42:55 -0500

    LeaderboardEntry javadocs

--- src/main/java/entity/leaderboard/LeaderboardEntry.java
+++ src/main/java/entity/leaderboard/LeaderboardEntry.java
@@ -1,4 +1,9 @@
 package entity.leaderboard;
 
+/**
+ * Generic class used to store a leaderboard entry
+ * @param username .
+ * @param score .
+ */
 public record LeaderboardEntry(String username, int score) {
 }
