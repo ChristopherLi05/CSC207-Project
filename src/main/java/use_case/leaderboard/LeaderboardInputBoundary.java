b50f69e952d68b57744a7189c7a332768f5bc1db
Hiro2974 <hiro.yano@utoronto.ca.mail>
2024-11-26 17:59:55 -0500

    added javadoc for leaderboard use_case

--- src/main/java/use_case/leaderboard/LeaderboardInputBoundary.java
+++ src/main/java/use_case/leaderboard/LeaderboardInputBoundary.java
@@ -1,5 +1,14 @@
 package use_case.leaderboard;
 
+/**
+ * The {@code LeaderboardInputBoundary} interface defines the method for interacting with the leaderboard use case.
+ * It serves as an entry point for actions that trigger changes or updates to the leaderboard data.
+ */
 public interface LeaderboardInputBoundary {
+
+    /**
+     * Executes the leaderboard use case, typically used to fetch and update the leaderboard data.
+     * This method could be responsible for loading the leaderboard, updating scores, or other related actions.
+     */
     void execute();
 }
