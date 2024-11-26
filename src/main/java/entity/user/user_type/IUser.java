6434adf5d9e9c9ae4a9018ec6f6f2a304e81b84e
Christopher Li <admissions.christopher@gmail.com>
2024-11-26 16:46:01 -0500

    IUser javadocs

--- src/main/java/entity/user/user_type/IUser.java
+++ src/main/java/entity/user/user_type/IUser.java
@@ -1,13 +1,41 @@
 package entity.user.user_type;
 
+/**
+ * Generic User Interface
+ */
 public interface IUser {
+    /**
+     * Gets the username of the user
+     *
+     * @return username
+     */
     String getUsername();
 
+    /**
+     * Gets the best score of the user
+     *
+     * @return best score
+     */
     int getBestScore();
 
+    /**
+     * Sets the best score of the user
+     *
+     * @param score score
+     */
     void setBestScore(int score);
 
+    /**
+     * Logged in state of the user (true if yes)
+     *
+     * @return login state
+     */
     boolean isLoggedIn();
 
+    /**
+     * Gets the session id of the user
+     *
+     * @return session id
+     */
     String getSessionId();
 }
