891b992aeaebe3159f4ac1d31934d147094b4f7b
Christopher Li <admissions.christopher@gmail.com>
2024-11-26 16:50:54 -0500

    UserManager javadocs

--- src/main/java/entity/user/UserManager.java
+++ src/main/java/entity/user/UserManager.java
@@ -2,6 +2,9 @@ package entity.user;
 
 import entity.user.user_type.IUser;
 
+/**
+ * Implementation of User Manager
+ */
 public class UserManager implements IUserManager {
     // Defaults to guest login
     private IUser currentUser;
@@ -16,26 +19,41 @@ public class UserManager implements IUserManager {
         this.currentUser = userFactory.createGuest();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public IUser getCurrentUser() {
         return currentUser;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void setUserLoggedIn(String sessionId, String name, int bestScore) {
         this.currentUser = userFactory.create(sessionId, name, bestScore);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void setUserGuest() {
         this.currentUser = userFactory.createGuest();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void setUserFactory(IUserFactory factory) {
         this.userFactory = factory;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public IUserFactory getUserFactory() {
         return userFactory;
