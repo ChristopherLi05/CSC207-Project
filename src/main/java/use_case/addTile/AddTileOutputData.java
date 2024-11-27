5a9b297c797402e7bdaa60eb931f4219ec50c81d
david-703 <jsdavid.won@gmail.com>
2024-11-26 19:31:35 -0500

    Added Javadocs for all methods in AddTileOutputData.

--- src/main/java/use_case/addTile/AddTileOutputData.java
+++ src/main/java/use_case/addTile/AddTileOutputData.java
@@ -12,31 +12,52 @@ public class AddTileOutputData {
     private final List<MahjongGroup> addedOpenGroup = new ArrayList<>();
 
     /**
-     * Constructs an AddTileOutputData object.
+     * Creates an AddTileOutputData object.
      */
     public AddTileOutputData() {
     }
 
+    /**
+     * Adds a tile to output data.
+     * @param tile the tile to be added
+     */
     public void addTile(MahjongTile tile) {
         this.addedTile.add(tile);
     }
 
+    /**
+     * Adds closed group to output data.
+     * @param mahjongGroup the closed group to be added
+     */
     public void addClosedGroup(MahjongGroup mahjongGroup) {
         this.addedClosedGroup.add(mahjongGroup);
     }
 
+    /**
+     * Adds open group to output data.
+     * @param mahjongGroup the open group to be added
+     */
     public void addOpenGroup(MahjongGroup mahjongGroup) {
         this.addedOpenGroup.add(mahjongGroup);
     }
 
+    /**
+     * @return the list of closed groups in output data
+     */
     public List<MahjongGroup> getAddedClosedGroup() {
         return addedClosedGroup;
     }
 
+    /**
+     * @return the list of open groups in output data
+     */
     public List<MahjongGroup> getAddedOpenGroup() {
         return addedOpenGroup;
     }
 
+    /**
+     * @return the list of added tiles in output data
+     */
     public List<MahjongTile> getAddedTile() {
         return addedTile;
     }
