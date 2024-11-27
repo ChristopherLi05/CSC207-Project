a1a0b0a9e6873d4a2e1f98feb154d18f7ed0ae3e
fuma <fuma.kano@mail.utoronto.ca>
2024-11-27 16:08:31 -0500

    Implemented rest of Calculator.java

--- src/main/java/entity/calculator/Calculator.java
+++ src/main/java/entity/calculator/Calculator.java
@@ -1,19 +1,15 @@
 package entity.calculator;
 
-import entity.calculator.mahjong.MahjongGroup;
-import entity.calculator.mahjong.MahjongSuit;
 import entity.calculator.mahjong.MahjongTile;
 import entity.calculator.yaku.*;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-
 /**
  * Static class to calculate hand values
  */
 public class Calculator {
-    private static final List<IYakuTestCase> yakuList = new ArrayList<IYakuTestCase>();
+    private static final List<IYakuTestCase> yakuList = new ArrayList<>();
 
     /**
      * Calculates han value of hand
@@ -21,7 +17,7 @@ public class Calculator {
      * @return han value
      */
     public static int calculateHan(HandState hand) {
-        return 0;
+        return 30;
     }
 
     /**
@@ -30,7 +26,17 @@ public class Calculator {
      * @return fu value
      */
     public static int calculateFu(HandState hand) {
-        return 0;
+        int result = 0;
+        if (hand.openGroups().isEmpty()) {
+            for (IYakuTestCase testCase : yakuList) {
+                result += testCase.getClosedHanValue();
+            }
+        } else {
+            for (IYakuTestCase testCase : yakuList) {
+                result += testCase.getOpenHanValue();
+            }
+        }
+        return result;
     }
 
     /**
@@ -39,7 +45,29 @@ public class Calculator {
      * @return score value
      */
     public static int calculateScore(HandState hand) {
-        return 0;
+        int fu = calculateFu(hand);
+        int basescore;
+        if (fu==1) {
+            basescore = 1000;
+        } else if (fu==2) {
+            basescore = 2000;
+        } else if (fu==3) {
+            basescore = 4000;
+        } else if (fu==4 || fu==5) {
+            basescore = 8000;
+        } else if (fu==6 || fu==7) {
+            basescore = 12000;
+        } else if (fu==8 || fu==9 || fu==10) {
+            basescore = 16000;
+        } else if (fu==11 || fu==12) {
+            basescore = 24000;
+        } else {
+            basescore = 32000;
+        }
+        if (hand.seatWind()==MahjongTile.EAST_WIND) {
+            return (int) Math.round(basescore * 1.5);
+        }
+        return basescore;
     }
 
     static {
