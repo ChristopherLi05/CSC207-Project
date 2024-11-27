286bc647f006d92822acd9f045d0c77003c4f148
fuma <fuma.kano@mail.utoronto.ca>
2024-11-27 01:53:27 -0500

    Implemented Javadoc for EastWindTestCase.java

--- src/main/java/entity/calculator/yaku/EastWindTestCase.java
+++ src/main/java/entity/calculator/yaku/EastWindTestCase.java
@@ -5,6 +5,13 @@ import entity.calculator.HandState;
 import entity.calculator.mahjong.MahjongGroup;
 import entity.calculator.mahjong.MahjongTile;
 
+/**
+ * Represents the East Wind Yaku test case.
+ *
+ * The East Wind Yaku is awarded when the hand contains a group of East Wind tiles
+ * and the round wind is also East Wind. This class implements the logic to verify
+ * the presence of this Yaku and provides its Han values for both closed and open hands.
+ */
 public class EastWindTestCase implements IYakuTestCase {
     @Override
     public boolean isYaku(HandState hand, HandGrouping handGrouping) {
@@ -18,11 +25,17 @@ public class EastWindTestCase implements IYakuTestCase {
     }
 
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int getClosedHanValue() {
         return 1;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int getOpenHanValue() { return 1; }
 }
