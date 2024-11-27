fe2b589d5be4b801017c45fc752e2541833ddf21
david-703 <jsdavid.won@gmail.com>
2024-11-26 19:21:54 -0500

    Added Javadocs for methods in AddTileInputBoundary.

--- src/main/java/use_case/addTile/AddTileInputBoundary.java
+++ src/main/java/use_case/addTile/AddTileInputBoundary.java
@@ -1,5 +1,9 @@
 package use_case.addTile;
 
 public interface AddTileInputBoundary {
+    /**
+     * Executes add tile operation with provided input data.
+     * @param inputData the data required to add the tile
+     */
     void execute(AddTileInputData inputData);
 }
