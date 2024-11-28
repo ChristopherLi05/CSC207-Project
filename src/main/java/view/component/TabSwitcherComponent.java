df1c898e31668a056d88860e1c311c1e5f93aa73
MaxChen102 <maxx.chen@mail.utoronto.ca>
2024-11-27 22:46:44 -0500

    "made some changes to tabswitcher such that the button names are different, also added logout button but not implented yet"

--- src/main/java/view/component/TabSwitcherComponent.java
+++ src/main/java/view/component/TabSwitcherComponent.java
@@ -13,6 +13,10 @@ import java.util.Map;
  */
 public class TabSwitcherComponent extends JPanel implements PropertyChangeListener {
     private final ViewManager viewManager;
+    private JButton calculator;
+    private JButton puzzleRush;
+    private JButton leaderboard;
+    private JButton logOut;
 
     public TabSwitcherComponent(ViewManager viewManager) {
         this.viewManager = viewManager;
@@ -23,27 +27,43 @@ public class TabSwitcherComponent extends JPanel implements PropertyChangeListen
     public void remakeButtons() {
         this.removeAll();
 
-        for (Map.Entry<String, ViewState<?>> pair : viewManager.getViewStates().entrySet()) {
-            if (pair.getValue().isTabswitcher()) {
-                this.add(createJButton(pair.getKey()));
-            }
-        }
+//        for (Map.Entry<String, ViewState<?>> pair : viewManager.getViewStates().entrySet()) {
+//            if (pair.getValue().isTabswitcher()) {
+//                this.add(createJButton(pair.getKey()));
+//            }
+//        }
+
+        calculator = new JButton("Calculator");
+        puzzleRush = new JButton("Puzzle Rush");
+        leaderboard = new JButton("Leaderboard");
+        logOut = new JButton("Log Out");
+
+        this.add(calculator);
+        this.add(puzzleRush);
+        this.add(leaderboard);
+        this.add(logOut);
+
+        calculator.addActionListener(e -> viewManager.setView("CalculatorView"));
+        puzzleRush.addActionListener(e -> viewManager.setView("PuzzleRushView"));
+        leaderboard.addActionListener(e -> viewManager.setView("LeaderboardView"));
+        // TODO: create logout use case
+//        logOut.addActionListener(e -> viewManager.setView("LogOutView"));
 
         this.validate();
         this.revalidate();
         this.repaint();
     }
 
-    /**
-     * Creates a JButton based on a view name and tells viewmanager to switch to that view
-     * @param viewName .
-     * @return created JButton
-     */
-    public JButton createJButton(String viewName) {
-        JButton button = new JButton(viewName);
-        button.addActionListener(e -> viewManager.setView(viewName));
-        return button;
-    }
+//    /**
+//     * Creates a JButton based on a view name and tells viewmanager to switch to that view
+//     * @param viewName .
+//     * @return created JButton
+//     */
+//    public JButton createJButton(String viewName) {
+//        JButton button = new JButton(viewName);
+//        button.addActionListener(e -> viewManager.setView(viewName));
+//        return button;
+//    }
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
