cc94bb42f0ffa63eee433b3cd685fe552a9a2c24
MaxChen102 <maxx.chen@mail.utoronto.ca>
2024-11-27 23:20:19 -0500

    "decided not to add a logout use case, just take user back to signup when log out is pressed"

--- src/main/java/view/component/TabSwitcherComponent.java
+++ src/main/java/view/component/TabSwitcherComponent.java
@@ -13,6 +13,7 @@ import java.util.Map;
  */
 public class TabSwitcherComponent extends JPanel implements PropertyChangeListener {
     private final ViewManager viewManager;
+
     private JButton calculator;
     private JButton puzzleRush;
     private JButton leaderboard;
@@ -27,12 +28,6 @@ public class TabSwitcherComponent extends JPanel implements PropertyChangeListen
     public void remakeButtons() {
         this.removeAll();
 
-//        for (Map.Entry<String, ViewState<?>> pair : viewManager.getViewStates().entrySet()) {
-//            if (pair.getValue().isTabswitcher()) {
-//                this.add(createJButton(pair.getKey()));
-//            }
-//        }
-
         calculator = new JButton("Calculator");
         puzzleRush = new JButton("Puzzle Rush");
         leaderboard = new JButton("Leaderboard");
@@ -46,24 +41,14 @@ public class TabSwitcherComponent extends JPanel implements PropertyChangeListen
         calculator.addActionListener(e -> viewManager.setView("CalculatorView"));
         puzzleRush.addActionListener(e -> viewManager.setView("PuzzleRushView"));
         leaderboard.addActionListener(e -> viewManager.setView("LeaderboardView"));
-        // TODO: create logout use case
-//        logOut.addActionListener(e -> viewManager.setView("LogOutView"));
+        logOut.addActionListener(e -> {
+            viewManager.setView("SignupView");
+        });
 
         this.validate();
         this.revalidate();
         this.repaint();
     }
-
-//    /**
-//     * Creates a JButton based on a view name and tells viewmanager to switch to that view
-//     * @param viewName .
-//     * @return created JButton
-//     */
-//    public JButton createJButton(String viewName) {
-//        JButton button = new JButton(viewName);
-//        button.addActionListener(e -> viewManager.setView(viewName));
-//        return button;
-//    }
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
