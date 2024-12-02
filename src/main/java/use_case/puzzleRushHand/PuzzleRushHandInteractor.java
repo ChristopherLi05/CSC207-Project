5c2ebc682d21f0d6e943a201506a130b0ebe2efe
Christopher Li <>
2024-12-02 10:43:52 -0500

    make puzzle rush work

--- src/main/java/use_case/puzzleRushHand/PuzzleRushHandInteractor.java
+++ src/main/java/use_case/puzzleRushHand/PuzzleRushHandInteractor.java
@@ -19,10 +19,11 @@ public class PuzzleRushHandInteractor implements PuzzleRushHandInputBoundary {
     private final String[] hands = {
             "1s1m2m3m4m5m6m7m8m9mgdgdgd   1s 9m  ew ew 1",  // 1500
             "2p2p3p3p4p4p5p5p5p6m  5p6p7p 6m 9m  ew ew 1",  // 1500
-            "1s1s1s2p3p4p5pwdwdwdgdgdgd   5p 6m 9m  ew ew 1", // 3000
+            "1s1s1s2p3p4p5pwdwdwdgdgdgd   5p 6m 9m ew ew 1", // 3000
             "1s1s1s2s2s2s3s3s3s4s5s6s7s   7s 6m 9m ew ew 1", // 18000
-            "1m1m1m2m2m2m3m3m3ms7m  4m5m6m 7m 6m 9m ew ew 1" // 12000
+            "1m1m1m2m2m2m3m3m3m7m  4m5m6m 7m 6m 9m ew ew 1" // 12000
     };
+    private int lastSeen = 0;
 
     /**
      * Constructs a new {@code PuzzleRushHandInteractor}.
@@ -45,7 +46,12 @@ public class PuzzleRushHandInteractor implements PuzzleRushHandInputBoundary {
     @Override
     public void execute(int timeLeft, int currScore) {
         // Generate a random hand from the predefined hands array.
-        String randomHand = hands[(int) (Math.random() * hands.length)];
+        int newState = (int) (Math.random() * hands.length);
+        if (newState == lastSeen) {
+            newState = (newState + 1 ) % hands.length;
+        }
+        lastSeen = newState;
+        String randomHand = hands[newState];
 
         // Create a hand state from the random hand string.
         var handState = handStateFactory.createHandState(randomHand);
